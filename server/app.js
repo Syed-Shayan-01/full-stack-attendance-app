6c3f4678e8b4e0d469a5e398c009ddf318e4b73f
Syed Shayan <shayansyed712@gmail.com>
2024-01-29 23:40:30 +0500

    add verification using jwt

--- server/app.js
+++ server/app.js
@@ -4,14 +4,17 @@ const cors = require('cors');
 const mongoose = require('./db/auth')
 const env = require('dotenv').config();
 const jwt = require('jsonwebtoken')
+var cookieParser = require('cookie-parser')
 const indexRouter = require('./routes/index');
 const authRouter = require('./routes/auth')
-var bodyParser = require('body-parser')
+var bodyParser = require('body-parser');
+const verifyToken = require('./middlewares/verify');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 app.use(express.json());
+app.use(cookieParser())
 app.use(cors());
-// app.use('/', indexRouter)
+app.use('/', verifyToken,indexRouter)
 
 app.use('/auth', authRouter)
 app.listen(process.env.PORT, () => {
--- server/middlewares/verify.js
+++ server/middlewares/verify.js
@@ -1,4 +1,16 @@
 const express = require('express');
 const jwt = require('jsonwebtoken')
-const { token } = req.headers;
-const verifyToken = jwt.verify()
+const verifyToken = (req, res, next) => {
+    const token = req.headers['authorization'];
+    console.log(token)
+    jwt.verify(token, process.env.SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send("err --->", err);
+        }
+        res.user = decoded;
+        next();
+    });
+}
+
+
+module.exports = verifyToken;
