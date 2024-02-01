4a700429b1661fab4e0bddb0e70d18ae15a36ec4
Syed Shayan <shayansyed712@gmail.com>
2024-02-01 23:36:25 +0500

    new changes in server side add attend Schema

---
+++ server/models/attendSchema.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+const attendScehma = new Schema({
+    name: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    course: { type: String, required: true },
+    phoneNumber: { type: String, required: true },
+})
+
+const Attend = mongoose.model('Attend', attendScehma);
+
+module.exports = Attend;
