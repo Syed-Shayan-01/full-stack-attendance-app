1c52a24cc42fb895b4a30a76bc79d076cc834123
Syed Shayan <shayansyed712@gmail.com>
2024-02-13 23:46:18 +0500

    new change

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -1,11 +1,22 @@
 import { Button } from "@/page/ui/button"
 import Link from "next/link"
+import { useEffect, useState } from "react";
 
 const Header = () => {
+    const [token, setToken] = useState("");
+
+    useEffect(() => {
+        const authToken = localStorage.getItem("token");
+        if (authToken) {
+            setToken(authToken);
+            console.log(authToken);
+
+        }
+    }, []);
     return (
         <div className="flex justify-between items-center mb-6">
             <h1 className="text-2xl font-bold">Students</h1>
-            <Link href={'/form'}><Button>Add Student</Button></Link>
+            {token && <Link href={'/form'}><Button>Add Student</Button></Link>}
         </div>
 
     )
--- client/src/components/navbar/Navbar.js
+++ client/src/components/navbar/Navbar.js
@@ -1,8 +1,15 @@
 import { LogOutIcon, SchoolIcon, TicketIcon } from "lucide-react"
 import Link from "next/link"
-
+import { useRouter } from "next/router"
 
 const Navbar = () => {
+    const router = useRouter();
+    const handleLogout = () => {
+            const tokenClear = localStorage.clear("token");
+            if (tokenClear) {
+                router.replace('/');
+            }
+    }
     return (
         <>
             <aside className=" md:w-64 bg-white p-6">
@@ -20,7 +27,7 @@ const Navbar = () => {
                     </a>
                     <a className="flex items-center space-x-2 text-gray-600" href="#">
                         <LogOutIcon className="h-6 w-6" />
-                        <span className="font-medium">Logout</span>
+                        <span onClick={handleLogout} className="font-medium">Logout</span>
                     </a>
                 </nav>
             </aside>
