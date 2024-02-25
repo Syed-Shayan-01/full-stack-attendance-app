d68ef3f719941c9cd24e59a17b1fe36f7d873f7d
Syed Shayan <shayansyed712@gmail.com>
2024-02-25 16:52:25 +0500

    new change

--- client/src/config/baseUrl.js
+++ client/src/config/baseUrl.js
@@ -1,3 +1,3 @@
-// const baseUrl = 'https://shiny-hoodie-calf.cyclic.app/';
-const baseUrl = 'http://localhost:4000/';
+const baseUrl = 'https://shiny-hoodie-calf.cyclic.app/';
+// const baseUrl = 'http://localhost:4000/';
 export default baseUrl;
