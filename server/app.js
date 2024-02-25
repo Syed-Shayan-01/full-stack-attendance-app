146359e9be8c56fb1df70d60199548d79cfbed7a
Syed Shayan <shayansyed712@gmail.com>
2024-02-25 17:31:35 +0500

    new changes

--- server/app.js
+++ server/app.js
@@ -16,12 +16,12 @@ app.use(bodyParser.json())
 app.use(cors());
 app.use((req, res, next) => {
     res.header(
-        Access - Control - Allow - Origin,
+        "Access - Control - Allow - Origin",
         "https://shayan-attend.vercel.app"
     );
-    res.header(Access - Control - Allow - Methods, "GET, POST, PUT, DELETE, OPTIONS");
-    res.header(Access - Control - Allow - Headers, "Content - Type, Authorization");
-    res.header(Access - Control - Allow - Credentials, true);
+    res.header("Access - Control - Allow - Methods", "GET, POST, PUT, DELETE, OPTIONS");
+    res.header("Access - Control - Allow - Headers", "Content - Type, Authorization");
+    res.header("Access - Control - Allow - Credentials", true);
 
     console.log("“Request received:”", req.method, req.url);
 
