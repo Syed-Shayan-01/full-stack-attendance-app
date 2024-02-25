c9d2c5976a7f672a21c720ea33b1f5232732cf86
Syed Shayan <shayansyed712@gmail.com>
2024-02-25 17:28:00 +0500

    new chnage

--- server/app.js
+++ server/app.js
@@ -17,7 +17,7 @@ app.use(cors());
 app.use((req, res, next) => {
     res.header(
         Access - Control - Allow - Origin,
-        "https://shiny-hoodie-calf.cyclic.app"
+        "https://shayan-attend.vercel.app"
     );
     res.header(Access - Control - Allow - Methods, "GET, POST, PUT, DELETE, OPTIONS");
     res.header(Access - Control - Allow - Headers, "Content - Type, Authorization");
