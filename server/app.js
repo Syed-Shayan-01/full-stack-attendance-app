4296a1506df3903c2fe7ce888773041708e86c07
Syed Shayan <shayansyed712@gmail.com>
2024-02-25 16:58:13 +0500

    new change in app .js

--- server/app.js
+++ server/app.js
@@ -13,11 +13,7 @@ var bodyParser = require('body-parser');
 const verifyToken = require('./middlewares/verify');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
-app.use(cors({
-    origin: ["https://shayan-attend.vercel.app"],
-    methods: ["GET", "POST", "PUT", "DELETE"],
-    credentials: true
-}));
+app.use(cors());
 
 app.use(express.json());
 app.use('/', indexRouter)
