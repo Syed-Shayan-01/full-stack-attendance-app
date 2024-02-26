c51a4f2842797b0c6756c97a60227b7d7f1dd652
Syed Shayan <shayansyed712@gmail.com>
2024-02-26 19:21:57 +0500

    new change

--- server/app.js
+++ server/app.js
@@ -14,20 +14,6 @@ const verifyToken = require('./middlewares/verify');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json())
 app.use(cors());
-app.use((req, res, next) => {
-    res.header(
-        "Access - Control - Allow - Origin",
-        "https://shayan-attend.vercel.app"
-    );
-    res.header("Access - Control - Allow - Methods", "GET, POST, PUT, DELETE, OPTIONS");
-    res.header("Access - Control - Allow - Headers", "Content - Type, Authorization");
-    res.header("Access - Control - Allow - Credentials", true);
-
-    console.log("“Request received:”", req.method, req.url);
-
-    next();
-});
-
 
 app.use(express.json());
 app.use('/', indexRouter)
