78211f2d4336ead9d10199f582c34a5ce662c2bf
Syed Shayan <shayansyed712@gmail.com>
2024-03-08 01:38:07 +0500

    Adds four jokes to the root route response.

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-
+// provide 4 sexy jokes 
+let jokeList = [
+    'Why did the scarecrow win an award? Because he was outstanding in his field!',
+    "What do you call a group of cows playing instruments? A moo-sical band.",
+    'Why don’t scientists trust atoms? Because they make up everything.',
+    'Why did the bicycle fall over? Because it was two-tired.'
+]
 router.get('/', (req, res) => {
-    res.send('Hello Shayan')
-})
 
-module.exports = router
+    res.send(jokeList);
+})
+module.exports = router;
